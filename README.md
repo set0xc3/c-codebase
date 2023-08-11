b9ed2fd5bf6ac8ecee4b5fe41647e66d5492ba47
duck <duck@pop-os.localdomain>
2023-08-11 04:07:59 +0300

    backup

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 <h1 align="center">C code base</h1>
 
+## Depts
+- sdl2
+- mingw-w64
+
 ## Build linux
 ```sheel
-zig build -Dtarget=x86_64-linux-gnu
+zig build -Dtarget=x86_64-native-gnu
 ```
 
 ## Build windows
 ```sheel
 zig build -Dtarget=x86_64-windows-gnu
+```
+
+## Run sandbox
+```sheel
+zig build run:sandbox
 ```
--- build.zig
+++ build.zig
@@ -34,26 +34,34 @@ const iflags = [_][]const u8{
 };
 
 pub fn platform_settings(step: *Step.Compile, target: CrossTarget) void {
-    if (target.os_tag.? == .linux) {
+    if (target.os_tag != null) {
+        if (target.os_tag.? == .linux) {
+            step.addIncludePath("/usr/include");
+            step.addLibraryPath("/usr/lib");
+            step.linkSystemLibrary("c");
+            step.linkSystemLibrary("m");
+            step.linkSystemLibrary("SDL2");
+        } else if (target.os_tag.? == .windows) {
+            step.addIncludePath("/usr/x86_64-w64-mingw32/include");
+            step.addLibraryPath("/usr/x86_64-w64-mingw32/bin");
+            step.addLibraryPath("/usr/x86_64-w64-mingw32/lib");
+            step.linkSystemLibrary("user32");
+            step.linkSystemLibrary("gdi32");
+            step.linkSystemLibrary("winmm");
+            step.linkSystemLibrary("setupapi");
+            step.linkSystemLibrary("oleaut32");
+            step.linkSystemLibrary("ole32");
+            step.linkSystemLibrary("imm32");
+            step.linkSystemLibrary("version");
+            step.linkSystemLibrary("SDL2");
+            step.linkSystemLibrary("SDL2main");
+        }
+    } else {
         step.addIncludePath("/usr/include");
         step.addLibraryPath("/usr/lib");
         step.linkSystemLibrary("c");
         step.linkSystemLibrary("m");
         step.linkSystemLibrary("SDL2");
-    } else if (target.os_tag.? == .windows) {
-        step.addIncludePath("/usr/x86_64-w64-mingw32/include");
-        step.addLibraryPath("/usr/x86_64-w64-mingw32/bin");
-        step.addLibraryPath("/usr/x86_64-w64-mingw32/lib");
-        step.linkSystemLibrary("user32");
-        step.linkSystemLibrary("gdi32");
-        step.linkSystemLibrary("winmm");
-        step.linkSystemLibrary("setupapi");
-        step.linkSystemLibrary("oleaut32");
-        step.linkSystemLibrary("ole32");
-        step.linkSystemLibrary("imm32");
-        step.linkSystemLibrary("version");
-        step.linkSystemLibrary("SDL2");
-        step.linkSystemLibrary("SDL2main");
     }
 }
 
